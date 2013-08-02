b8566cfd899468cf54105517ea4700d8130d3059
Gavin Black <gblack@mitre.org>
2013-08-02 16:58:46 -0400

    Switched to IMSHandler framework

---
+++ IMSHandler/IMSHandler.h
@@ -0,0 +1,13 @@
+//
+//  IMSHandler.h
+//  Memory Security Demo
+//
+//  Created by Black, Gavin S. on 8/2/13.
+//  Copyright (c) 2013 Black, Gavin S. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IMSHandler : NSObject
+
+@end
---
+++ IMSHandler/IMSHandler.m
@@ -0,0 +1,13 @@
+//
+//  IMSHandler.m
+//  Memory Security Demo
+//
+//  Created by Black, Gavin S. on 8/2/13.
+//  Copyright (c) 2013 Black, Gavin S. All rights reserved.
+//
+
+#import "IMSHandler.h"
+
+@implementation IMSHandler
+
+@end
